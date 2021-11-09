265abb4001c9897684a131ddbd202b20e8723ce6
unknown <raveen.anand@whitehatjr.com>
2021-11-09 18:42:22 +0530

    v2

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -1,26 +1,17 @@
 var colors = ["white","orange","red","green"];
 
 function setup() {
-  //size of the canvas to draw on
   createCanvas(innerWidth, innerHeight);
-
-  // Set the background to white, and move the line to setup();
   background("white");
-
 }
 
-
 function draw() {
-  
   var y =50;
   for (i = 0; i < colors.length; i++){
-    //make an object for each color:
      currentColor = new Ball(150, y, 25, colors[i]);
      currentColor.appear();
      y = y + 50;
  }
-  
-    
 }
     
 
